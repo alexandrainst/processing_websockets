e674c201fb02470df147a5f06d7106d21008477e
Abe Pazos <abe@hamoid.com>
2019-11-25 18:16:33 +0100

    Move files into right folder matching package

--- src/websockets/WebsocketServer.java
+++ src/websockets/WebsocketServer.java
@@ -10,7 +10,7 @@ import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 import processing.core.PApplet;
 
 /**
- * 
+ *
  * @author Lasse Steenbock Vestergaard
  *
  */
@@ -19,11 +19,11 @@ public class WebsocketServer {
 	private Method websocketServerEvent;
 	private Method websocketServerEventBinary;
 	private WebsocketServerController serverController;
-	
+
 	/**
-	 * 
+	 *
 	 * The websocket server object that is initiated directly in the Processing sketch
-	 * 
+	 *
 	 * @param parent Processings PApplet object
 	 * @param port The port number you want the websocket server to initiate its connection on
 	 * @param uri The uri you want your server to respond to. Ex. /john (if the port is set to ex. 8025, then the full URI would be ws://localhost:8025/john).
@@ -31,25 +31,25 @@ public class WebsocketServer {
 	public WebsocketServer(PApplet parent, int port, String uri){
 		this.parent=parent;
 		this.parent.registerMethod("dispose", this);
-		
+
 		try {
         	websocketServerEvent = parent.getClass().getMethod("webSocketServerEvent", String.class);
 					websocketServerEventBinary = parent.getClass().getMethod("webSocketServerEvent", byte[].class, int.class, int.class);
         } catch (Exception e) {
         	// no such method, or an error.. which is fine, just ignore
         }
-		
+
 		Server server = new Server(port);
 		serverController = new WebsocketServerController(parent, websocketServerEvent, websocketServerEventBinary);
-		 
+
 		WebSocketHandler wsHandler = new WebSocketHandler(){
-			
+
 			 	@Override
 			    public void configure(WebSocketServletFactory factory){
 			        factory.setCreator(new WebsocketServerCreator(serverController));
 			    }
         };
- 
+
         ContextHandler contextHandler = new ContextHandler(uri);
 	    contextHandler.setAllowNullPathInfo(true); // disable redirect from /ws to /ws/
 	    contextHandler.setHandler(wsHandler);
@@ -62,17 +62,17 @@ public class WebsocketServer {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * This method is used for sending messages to all connected clients. This method will be updated with the possibility for sending messages to specific clients!
-	 * 
+	 *
 	 * @param message The message you want to send to all clients
 	 */
 	//TODO: Add possibility for sending back to specific client
 	public void sendMessage(String message){
 		serverController.writeAllMembers(message);
-		
+
 	}
 
 	public void sendMessage(byte[] data){
@@ -80,7 +80,7 @@ public class WebsocketServer {
 	}
 
 	public void dispose(){
-		// Anything in here will be called automatically when 
+		// Anything in here will be called automatically when
 	    // the parent sketch shuts down. For instance, this might
 	    // shut down a thread used by this library.
 	}
